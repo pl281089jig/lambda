7f32b31bc75017b7fbb6bcf9a47e24d0d99cdf81
pl281089jig <igor.jalov@ukr.net>
2016-05-16 17:08:16 +0300

    responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 ;(function(){
+	var body = document.body,
+		btn	= body.querySelector('.ba-toggle'); /*возвращаем елемент у которого есть класс ba-toggle*/
+		overlay = body.querySelector('.ba-overlay');
 
+	btn.onclick = function () {
+		body.classList.toggle('ba-menu-open');
+	}
+	overlay.onclick = function () {
+		body.classList.toggle('ba-menu-open');
+	}
 })();
